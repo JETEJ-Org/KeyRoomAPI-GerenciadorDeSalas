8f67a64d78e3e1437ab4ff104030069781d5203a
Eike M. <eike.miranda@acad.ifma.edu.br>
2024-12-17 11:25:16 -0300

    Update

--- src/controllers/reservaController.js
+++ src/controllers/reservaController.js
@@ -3,20 +3,22 @@ import Sala from '../models/salaModels.js';
 
 export async function listarReservas(req, res) {
     try {
-        const reservas = await Reserva.find();
+        const reservas = await Reserva.find().populate('sala_id');
         res.status(200).json(reservas);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
+
 export async function novaReserva(req, res) {
     try {
         const { sala_id, pessoa, data, horario_inicio, horario_termino, detalhes } = req.body;
 
-        const sala = await Sala.findById(sala_id);
+        const sala = await Sala.findOne({ _id: sala_id });
 
         if (!sala) return res.status(404).json({ message: 'Sala não encontrada' });
+
         const novaReserva = new Reserva({ sala_id, pessoa, data, horario_inicio, horario_termino, detalhes });
         const reservaCriada = await novaReserva.save();
         res.status(201).json(reservaCriada);
@@ -48,24 +50,26 @@ export async function atualizarReserva(req, res) {
         const reserva = await Reserva.findById(id);
         if (!reserva) return res.status(404).json({ message: 'Reserva não encontrada' });
 
-        const sala = await Sala.findById(reserva.sala_id);
+        const sala = await Sala.findOne({ _id: reserva.sala_id });
         if (!sala) return res.status(404).json({ message: 'Sala não encontrada' });
 
-        const reservaAtualizada = await Reserva.findByIdAndUpdate(id, { pessoa, data, horario_inicio, horario_termino, detalhes }, { new: true, runValidators: true });
-        if (reservaAtualizada) {
-            res.status(200).json(reservaAtualizada);
-        } else {
-            res.status(404).json({ message: 'Reserva não encontrada' });
-        }
+        const reservaAtualizada = await Reserva.findByIdAndUpdate(
+            id,
+            { pessoa, data, horario_inicio, horario_termino, detalhes },
+            { new: true, runValidators: true }
+        );
+        res.status(200).json(reservaAtualizada);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
 }
 
+
 export async function verificarConflito(req, res, next) {
     const { sala_id, data, horario_inicio, horario_termino } = req.body;
-    const inicio = new Date(`${data}T${horario_inicio}`);
-    const termino = new Date(`${data}T${horario_termino}`);
+
+    const inicioReservaNova = new Date(`${data}T${horario_inicio}`);
+    const terminoReservaNova = new Date(`${data}T${horario_termino}`);
 
     try {
         const reservas = await Reserva.find({ sala_id, data });
@@ -75,12 +79,11 @@ export async function verificarConflito(req, res, next) {
         }
 
         const conflito = reservas.some(reserva => {
-            const inicioReserva = new Date(`${reserva.data}T${reserva.horario_inicio}`);
-            const terminoReserva = new Date(`${reserva.data}T${reserva.horario_termino}`);
+            const inicioReservaExistente = new Date(`${reserva.data}T${reserva.horario_inicio}`);
+            const terminoReservaExistente = new Date(`${reserva.data}T${reserva.horario_termino}`);
 
             return (
-                reserva.sala_id.toString() === sala_id.toString() &&
-                !(termino <= inicioReserva || inicio >= terminoReserva)
+                (inicioReservaNova < terminoReservaExistente && terminoReservaNova > inicioReservaExistente)
             );
         });
 
